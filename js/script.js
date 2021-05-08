60551975bb867652700bb81c783decec2f01fa49
navneetkaur3@gmail.com <navneetkaur3@gmail.com>
2021-05-08 14:27:58 -0400

    Commit to Master , testing github from UI

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const mainEl = document.querySelector('main');
+
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+
+mainEl.classList.add('flex-ctr');
+
+//testing
